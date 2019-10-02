6d26808ab7ea5dc6ae10062ea135c774411bbcfd
Ana Silva <ananunesdasilva@gmail.com>
2019-10-02 22:32:31 +0100

    Adding a message to the messages list

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,9 +1,29 @@
 export const FETCH_MESSAGES = 'FETCH_MESSAGES';
+export const CREATE_MESSAGE = 'CREATE_MESSAGE';
 
 export const fetchMessages = () => {
   const promise = fetch('https://wagon-chat.herokuapp.com/general/messages').then(response => response.json());
   return {
     type: FETCH_MESSAGES,
+    payload: promise
+  };
+};
+
+export const createMessage = (channel, author, content) => {
+  const body = { author, content };
+  const url = `https://wagon-chat.herokuapp.com/${channel}/messages`
+
+  const promise = fetch(url, {
+    method: 'POST',
+    headers: {
+      Accept: 'application/json',
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(body)
+  }).then(r => r.json());
+
+  return {
+    type: CREATE_MESSAGE,
     payload: promise
   };
 };
---
+++ src/containers/message_form.jsx
@@ -0,0 +1,51 @@
+import { bindActionCreators } from 'redux';
+import { connect } from 'react-redux';
+import React, { Component } from 'react';
+import { createMessage } from '../actions';
+
+function mapDispatchToProps(dispatch) {
+  return bindActionCreators(
+    { createMessage },
+    dispatch
+  );
+}
+
+function mapStateToProps(state) {
+  return {
+    currentUser: state.currentUser
+  };
+}
+
+class MessageForm extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { value: '' };
+
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+
+  handleSubmit = (event) => {
+    event.preventDefault();
+    this.props.createMessage('general', this.props.currentUser, this.state.value);
+    this.setState({ value: '' });
+  }
+
+  handleChange = (event) => {
+    this.setState({ value: event.target.value });
+  }
+
+  render() {
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <label>
+          Message:
+          <input type="text" value={this.state.value} onChange={this.handleChange} />
+        </label>
+        <input type="submit" value="Submit" />
+      </form>
+    );
+  }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(MessageForm);
