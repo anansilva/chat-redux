777bf705c38f0bab4ecbcc2e0d589aa3ffbcdb4d
Ana Silva <ananunesdasilva@gmail.com>
2019-10-05 17:40:16 +0100

    Scroll down to the last message when updating the DOM

--- src/containers/message_list.jsx
+++ src/containers/message_list.jsx
@@ -26,13 +26,17 @@ class MessageList extends Component {
     this.refresher = setInterval(this.props.fetchMessages, 5000);
   }
 
+  componentDidUpdate() {
+    this.list.scrollTop = this.list.scrollHeight;
+  }
+
   componentWillUnmount() {
     clearInterval(this.refresher);
   }
 
   render() {
     return (
-      <div className="messages">
+      <div className="messages" ref={(list) => { this.list = list; }}>
         {this.props.messages.map(message => <Message message={message} key={message.id} />)}
       </div>
     );
